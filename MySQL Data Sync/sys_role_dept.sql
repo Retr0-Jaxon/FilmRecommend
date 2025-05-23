03650b297b1af7d581244dfabd67502ef0f064e3
Retr0_Jaxon <james2060533824@gmail.com>
2025-05-24 01:07:24 +0800

    sql data

---
+++ MySQL Data Sync/sys_role_dept.sql
@@ -0,0 +1,3 @@
+INSERT INTO ry_vue.sys_role_dept (role_id, dept_id) VALUES (2, 100);
+INSERT INTO ry_vue.sys_role_dept (role_id, dept_id) VALUES (2, 101);
+INSERT INTO ry_vue.sys_role_dept (role_id, dept_id) VALUES (2, 105);
